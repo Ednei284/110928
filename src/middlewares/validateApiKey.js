d947c81f7973bd83315cfc36031546d5c0d1627d
Ednei284 <metal2024heavy@gmail.com>
2026-07-31 00:14:17 -0300

    updade validkey

--- src/middlewares/validateApiKey.js
+++ src/middlewares/validateApiKey.js
@@ -2,8 +2,7 @@ const VALID_API_KEYS = process.env.VALID_API_KEYS; // Use .env!
 
 export default function validateApiKey(req, res, next) {
   const apiKey = req.headers['x-api-key'];
-
-  if (!apiKey || !VALID_API_KEYS.includes(apiKey)) {
+  if (apiKey !== VALID_API_KEYS) {
     return res.status(401).json({
       error: 'Acesso não autorizado. API Key inválida.'
     });
--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import { apiLimiter } from './middlewares/rateLimit.js';
-// import validateApiKey from './middlewares/validateApiKey.js';
+import validateApiKey from './middlewares/validateApiKey.js';
 import authRoutes from './routes/authRoutes.js';
 import userRoutes from './routes/userRoutes.js';
 import postsRoutes from './routes/postsRoutes.js';
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Limite global de API
-// app.use('/api', validateApiKey);
+app.use('/api', validateApiKey);
 app.use('/api', apiLimiter);
 
 // Rotas
