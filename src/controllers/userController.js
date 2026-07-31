499420256f7b15c82699794b9d6c9515b509e1d0
Ednei284 <metal2024heavy@gmail.com>
2026-07-31 20:43:52 -0300

    update userController

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -27,23 +27,27 @@ export const getProfile = async (req, res) => {
 
 export const updateProfile = async (req, res) => {
   try {
-    const { name, email, password } = req.body;
-
+    const { name, email, newPassword } = req.body;
+    const user = await prisma.user.update({
+      where: { email }
+    });
+    if (user.email === email) res.status(401).json({ error: 'Email ja casatrado.' })
+    if (user.name === name) res.status(401).json({ error: 'Nome ja casatrado.' })
     const updatedData = {
       name,
-      email
+      email,
+      password: newPassword
     };
-
-    if (password) {
-      updatedData.password = await bcrypt.hash(password, 10);
+    if (newPassword) {
+      updatedData.password = await bcrypt.hash(newPassword, 10);
     }
-
-    const user = await prisma.user.update({
+    if (user.password === await bcrypt.compare(updatedData.password, user.password)) res.status(401).json({ error: 'Nome ja casatrado.' })
+    const newUser = await prisma.user.update({
       where: { id: req.userId },
       data: updatedData
     });
 
-    res.json(user);
+    res.json(newUser);
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'Erro ao atualizar perfil' });
