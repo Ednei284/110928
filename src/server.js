f3e0a98aa99e5ddd9e0eaa034758694d9f0079b2
Ednei284 <metal2024heavy@gmail.com>
2026-08-11 03:41:38 -0300

    50mb

--- src/server.js
+++ src/server.js
@@ -14,8 +14,8 @@ const PORT = process.env.PORT || 3000;
 
 // Middlewares
 app.use(cors());
-app.use(express.json({ limit: '100mb' }));
-app.use(express.urlencoded({ limit: '100mb', extended: true }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 // Limite global de API
 app.use('/api', validateApiKey);
 app.use('/api', apiLimiter);
